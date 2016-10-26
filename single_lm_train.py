96f4bb27443bedc6c94de9bd755f7a0236a96972
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-10-26 13:28:50 +0300

    Run on any corpus

--- single_lm_train.py
+++ single_lm_train.py
@@ -4,13 +4,17 @@ from data_utils import Vocabulary, Dataset
 from language_model import LM
 from run_utils import run_train, run_eval
 
+
 flags = tf.flags
-flags.DEFINE_string("logdir", "/tmp/lm1b", "Logging directory.")
-flags.DEFINE_string("datadir", None, "Logging directory.")
-flags.DEFINE_string("mode", "train", "Whether to run 'train' or 'eval' model.")
-flags.DEFINE_string("hpconfig", "", "Overrides default hyper-parameters.")
-flags.DEFINE_integer("num_gpus", 1, "Number of GPUs used.")
-flags.DEFINE_integer("eval_steps", 70, "Number of eval steps.")
+flags.DEFINE_string('logdir', '/tmp/lm1b', 'Logging directory.')
+flags.DEFINE_string('datadir', None, 'Data directory.')
+flags.DEFINE_string('vocab', None, 'Vocab file.')
+flags.DEFINE_string(
+    'mode', 'train',
+    'Whether to run "train" or "eval_(train|valid|test)" model.')
+flags.DEFINE_string('hpconfig', '', 'Overrides default hyper-parameters.')
+flags.DEFINE_integer('num_gpus', 1, 'Number of GPUs used.')
+flags.DEFINE_integer('eval_steps', 70, 'Number of eval steps.')
 
 FLAGS = flags.FLAGS
 
@@ -19,20 +23,25 @@ def main(_):
     hps = LM.get_default_hparams().parse(FLAGS.hpconfig)
     hps.num_gpus = FLAGS.num_gpus
 
-    vocab = Vocabulary.from_file("1b_word_vocab.txt")
+    vocab = Vocabulary.from_file(FLAGS.vocab)
+    hps.vocab_size = vocab.num_tokens
 
-    if FLAGS.mode == "train":
+    if FLAGS.mode == 'train':
         hps.batch_size = 256
-        dataset = Dataset(vocab, FLAGS.datadir + "/training-monolingual.tokenized.shuffled/*")
-        run_train(dataset, hps, FLAGS.logdir + "/train", ps_device="/gpu:0")
-    elif FLAGS.mode.startswith("eval_"):
-        if FLAGS.mode.startswith("eval_train"):
-            data_dir = FLAGS.datadir + "/training-monolingual.tokenized.shuffled/*"
+        dataset = Dataset(vocab, FLAGS.datadir + '/train-*')
+        run_train(dataset, hps, FLAGS.logdir + '/train', ps_device='/gpu:0')
+    elif FLAGS.mode.startswith('eval_'):
+        if FLAGS.mode == 'eval_train':
+            data_dir = FLAGS.datadir + '/train-*'
+        elif FLAGS.mode == 'eval_valid':
+            data_dir = FLAGS.datadir + '/valid*'
+        elif FLAGS.mode == 'eval_test':
+            data_dir = FLAGS.datadir + '/test*'
         else:
-            data_dir = FLAGS.datadir + "/heldout-monolingual.tokenized.shuffled/news.en.heldout-00000-of-00050"
+            raise ValueError('Unknown mode {}'.format(FLAGS.mode))
         dataset = Dataset(vocab, data_dir, deterministic=True)
         run_eval(dataset, hps, FLAGS.logdir, FLAGS.mode, FLAGS.eval_steps)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tf.app.run()
