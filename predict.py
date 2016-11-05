6cf4c89d8959ce49ece875fce484d9a22c1ad5cc
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-11-05 23:21:50 +0300

    Extract function, do not hard-code size

--- predict.py
+++ predict.py
@@ -25,23 +25,27 @@ class Model:
         n = len(tokens) + 1
         xs[0, :n] = ([self.vocabulary.s_id] +
                      list(map(self.vocabulary.get_id, tokens)))
-        feed_dict = {self.input_xs: xs, self.batch_size: 1}
-        for v in tf.get_collection('initial_state'):
-            # FIXME - well, that's not very general/portable
-            state_size = 512 + 2048  # FIXME - epsecially this!
-            self.session.run(
-                tf.assign(v, tf.zeros([1, state_size]), validate_shape=False))
+        batch_size = 1
+        self._init_state(batch_size)
+        feed_dict = {self.input_xs: xs, self.batch_size: batch_size}
         return self.session.run(self.softmax[n - 1], feed_dict=feed_dict)
+
+    def _init_state(self, batch_size):
+        # FIXME - well, that's not very general/portable
+        # (especially the way we get the shape, but zeros too)
+        proj_w = [v for v in tf.all_variables()
+                  if v.name == 'model/lstm_0/LSTMCell/W_P_0:0'][0]
+        state_size, proj_size = proj_w.get_shape()
+        for v in tf.get_collection('initial_state'):
+            self.session.run(
+                tf.assign(v, tf.zeros([batch_size, state_size + proj_size]),
+                          validate_shape=False))
 
     def predict_top(self, tokens: List[str], top=10) -> List[Tuple[str, float]]:
         probs = self.predict(tokens)
         top_indices = argsort_k_largest(probs, top)
         return [(self.vocabulary.get_token(id_), probs[id_])
                 for id_ in top_indices]
-
-    def __del__(self):
-        if hasattr(self, 'session'):
-            self.session.__exit__(None, None, None)
 
 
 def argsort_k_largest(x, k):
